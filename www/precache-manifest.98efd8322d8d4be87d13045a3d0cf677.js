4c8ad083a27604784f298014e6c62c83196e0f14
Kisoong Jang <kisjang@kr.ibm.com>
2019-04-15 10:48:01 +0900

    update

--- www/precache-manifest.98efd8322d8d4be87d13045a3d0cf677.js
+++ www/precache-manifest.98efd8322d8d4be87d13045a3d0cf677.js
@@ -16,31 +16,31 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "fonts/MaterialIcons-Regular.woff2"
   },
   {
-    "revision": "9fa40ac188452505f216044f8c7a9b88",
-    "url": "static/icons/512x512.png"
-  },
-  {
     "revision": "c88219dd9796cca0bfa90e472a805ab9",
     "url": "static/icons/favicon.png"
-  },
-  {
-    "revision": "ad809d0a19d880fc189e41e9726df58e",
-    "url": "static/icons/256x256.png"
   },
   {
     "revision": "14235e3018bb57cc700c799c5f9ef1be",
     "url": "static/icons/apple-touch-icon.png"
   },
   {
+    "revision": "9fa40ac188452505f216044f8c7a9b88",
+    "url": "static/icons/512x512.png"
+  },
+  {
+    "revision": "ad809d0a19d880fc189e41e9726df58e",
+    "url": "static/icons/256x256.png"
+  },
+  {
+    "revision": "09bac65cbc73f494482c480006f7c31a",
+    "url": "static/icons/152x152.png"
+  },
+  {
     "revision": "1c41296078623fe115c3b966852a9759",
     "url": "static/icons/192x192.png"
   },
   {
-    "revision": "b08f600e75284d6274ee0ee03f2ed1b3",
-    "url": "static/icons/128x128.png"
-  },
-  {
-    "revision": "63c39419dabde6c87105",
+    "revision": "5154ce8c60c40524de8c",
     "url": "js/app.js"
   },
   {
@@ -48,8 +48,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "static/icons/144x144.png"
   },
   {
-    "revision": "09bac65cbc73f494482c480006f7c31a",
-    "url": "static/icons/152x152.png"
+    "revision": "b08f600e75284d6274ee0ee03f2ed1b3",
+    "url": "static/icons/128x128.png"
   },
   {
     "revision": "012cf6a10129e2275d79d6adac7f3b02",
@@ -80,7 +80,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "fonts/Framework7Icons-Regular.eot"
   },
   {
-    "revision": "63c39419dabde6c87105",
+    "revision": "5154ce8c60c40524de8c",
     "url": "css/app.css"
   }
 ]);
